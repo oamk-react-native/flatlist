c7950ed1e5a426a67852a33dae3634ef2f42ba7d
Jouni Juntunen <jouni.juntunen@oamk.fi>
2021-02-05 13:54:01 +0200

    Search implemented.

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { FlatList, SafeAreaView, StyleSheet } from 'react-native';
+import React,{useState,useEffect} from 'react';
+import { FlatList, SafeAreaView, StyleSheet, TextInput, View } from 'react-native';
 import {DATA} from './Data';
 import Row from './Row';
+import Search from './Search';
 
 export default function App() {
-  
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    setItems(DATA);
+  }, [])
+
+
   /*
   function renderItem({item}) {
     return  (<Text>{item.lastname}</Text>);
@@ -17,10 +24,16 @@ export default function App() {
   );
   */
 
+  const executeSearch = (search) => {
+    const searchArray = DATA.filter((item) => item.lastname.startsWith(search));
+    setItems(searchArray);
+  }
+
   return (
     <SafeAreaView style={styles.container}>
+      <Search executeSearch={executeSearch} />
       <FlatList
-        data={DATA}
+        data={items}
         renderItem = {({item}) => (
           <Row person={item} />
         )}
@@ -33,6 +46,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    margin: 10
   },
 });
 
--- Row.js
+++ Row.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 
 export default function Row({person}) {
   return (
-    <Text>{person.lastname}, {person.firstname}</Text>
+    <Text style={styles.row}>{person.lastname}, {person.firstname}</Text>
   )
 }
+
+const styles = StyleSheet.create({
+  row: {
+    marginBottom: 3,
+    backgroundColor: '#f5f5f5',
+    padding: 5,
+  }
+});
